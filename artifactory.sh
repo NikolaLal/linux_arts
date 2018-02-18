429cc8b649e273a082918a66ebd19f917422ecf9
NikolaLal <niklalatovic@gmail.com>
2018-02-18 15:45:04 +0100

    Update artifactory.sh

--- artifactory.sh
+++ artifactory.sh
@@ -1,13 +1,18 @@
-# Install Java
 sudo apt-get -y update
 sudo apt-get install -y $1
 sudo apt-get -y update --fix-missing
 sudo apt-get install -y $1
 
-# Install tomcat
-sudo apt-get install -y  $2
+sudo echo "deb https://jfrog.bintray.com/artifactory-debs xenial main" | sudo tee -a /etc/apt/sources.list
 
-if netstat -tulpen | grep 8080
+sudo curl https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install jfrog-artifactory-oss
+
+sudo systemctl start artifactory.service
+
+if netstat -tulpen | grep 8081
 then
 	exit 0
 fi
